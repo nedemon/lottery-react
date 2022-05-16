9893263f5d19ee63109f733ea40098cdc7f618dd
Sergey Tsvetkov <sergey.tsvetkov@invent.us>
2022-05-16 20:31:31 +0300

    initial commit

---
+++ src/lottery.js
@@ -0,0 +1,51 @@
+import web3 from './web3';
+
+const address = '0x8dFD29BC067ea7aDCeE8eA4aDe0892f0e164B634';
+const abi = [
+  { inputs: [], stateMutability: 'nonpayable', type: 'constructor' },
+  {
+    inputs: [],
+    name: 'enter',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function'
+  },
+  {
+    inputs: [],
+    name: 'getPlayers',
+    outputs: [ { internalType: 'address[]', name: '', type: 'address[]' } ],
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [],
+    name: 'logWinner',
+    outputs: [ { internalType: 'address', name: '', type: 'address' } ],
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [],
+    name: 'manager',
+    outputs: [ { internalType: 'address', name: '', type: 'address' } ],
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    inputs: [],
+    name: 'pickWinner',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    inputs: [ { internalType: 'uint256', name: '', type: 'uint256' } ],
+    name: 'players',
+    outputs: [ { internalType: 'address', name: '', type: 'address' } ],
+    stateMutability: 'view',
+    type: 'function'
+  }
+]
+;
+
+export default new web3.eth.Contract(abi, address);
